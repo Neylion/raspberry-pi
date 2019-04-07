11a6430e3bc37c123c252831edd98a3918dc70c7
Fredrik Lindberg <fredrik.j.lindberg@gmail.com>
2019-04-07 11:30:06 +0000

    Added clone script

---
+++ 01 clone.sh
@@ -0,0 +1,8 @@
+# Go to the user folder for the user "pi"
+cd /home/pi
+# Create a new folder for our git repositories
+mkdir git
+# Go to the repos
+cd git
+# Clone the "raspberry-pi" repository
+git clone https://github.com/Neylion/raspberry-pi.git
